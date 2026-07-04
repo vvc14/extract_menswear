606a291cf91dfe816378400f582f69507f6fbe12
Vyaswanth Velchuri <velchuri8885405535@gmail.com>
2026-07-04 21:17:03 +0530

    fix: resource fail on first try bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'http://127.0.0.1:5000',
     },
     allowedHosts: [
       'chastise-green-hesitancy.ngrok-free.',
