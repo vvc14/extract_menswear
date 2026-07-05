a42b45d5704b23a97a7cce3c743fa808241c8a56
ymp7 <ymonishprasanna@gmail.com>
2026-07-05 21:45:13 +0530

    My ngrok

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': 'http://127.0.0.1:5000',
     },
     allowedHosts: [
-      'chastise-green-hesitancy.ngrok-free.',
+      'chastise-green-hesitancy.ngrok-free.dev',
       'mossy-roast-moonlight.ngrok-free.dev',
     ],
   },
